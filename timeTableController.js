3f1530d1a98aac9b21117ede9d63a89244268579
Adomiraruu <samaranayake.rishen@gmail.com>
2024-02-29 00:20:43 +0530

    Added delete timetable function

--- timeTableController.js
+++ timeTableController.js
@@ -23,13 +23,13 @@ const createTimeTable = async(req,res)=>{
 const updateTimetable = async(req, res) => {
     try {
         const { id } = req.params;
-        const { tutorialGroup, day, sessions } = req.body; //Get updated data
 
-        const updatedTimetable  = await timeTableModel.findAndUpdate(
+        const { tutorialGroup, day, sessions } = req.body; //Get updated data
+        const updatedTimetable  = await timeTableModel.findByIdAndUpdate(
             id,
             { tutorialGroup, day, sessions },
-            { new: true}
-        );
+            { new: true} //Return updated version
+        )
 
         if (!updateTimetable) {
             return res.status(404).json({
@@ -38,7 +38,42 @@ const updateTimetable = async(req, res) => {
             });
         }
 
-        
+        res.status(200).json({
+            status: "success",
+            data: {
+                timetable: updateTimetable,
+            },
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: error.message,
+        })
     }
-}
-module.exports={createTimeTable};
+};
+
+const deleteTimetable = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const deletedTimetable = await timeTableModel.findByIdAndDelete(id);
+        if (!deletedTimetable) {
+            return res.status(404).json({
+                status: "error",
+                message: "Timetable not found",
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            message: "Timetable deleted successfully",
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+};
+
+module.exports={createTimeTable, updateTimetable, deleteTimetable};
