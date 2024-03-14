7fa5d69d16e2e32a4ed4ed4ed13c5133097da196
Adomiraruu <samaranayake.rishen@gmail.com>
2024-03-14 10:35:09 +0530

    Cleaned up branch

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const morgan = require("morgan");
-const moment = require('moment-timezone');
-const cors = require('cors');
 const attendanceRouter = require('./routes/attendanceRoutes');
 const timetableRouter = require('./routes/timeTableRoutes');
 const cors = require('cors');
 const port = 5173;
 
-
-moment.tz.setDefault('Asia/Colombo');
 const app = express();
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
-
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({
