6553e43759b4958251d2eeb23f229c6937c0f323
Nethmini Apsara <nethminiapsara2022@gmail.com>
2024-03-17 14:23:18 +0530

    Modified userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,38 +1,26 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: {
+
+  firstName: {
     type: String,
-    required: [true, "A user must have an email"],
+    required: [true, "A user must have an first name"],
   },
 
-  uowId: {
+  lastName: {
     type: String,
-    required: [true, "A user must have a UOW ID"],
-  },
-  iitId:{
-    type: String,
-    required: [true, "A user must have a IIT ID"],
+    required: [true, "A user must have an last name"],
   },
 
-  tutorialGroup: {
-    type: String,
-    enum: ["A", "B", "C", "D"],
-    required: [true, "A user must have a tutorial group"],
-  },
-  degreeType: {
-    type: String,
-    enum: ["SE", "CS", "BIS"],
-    required: [true, "A user must have a degree type"],
-  },
-  year: {
-    type: String,
-    enum: ["L4", "L5", "L6"],
-    required: [true, "A user must have a year"],
-  },
   uid: {
     type: String,
     required: [true, "A user must have a uid"],
+  },
+
+  role: {
+    type: String,
+    enum: ['student', 'lecturer'],
+    required: true
   },
 });
 
